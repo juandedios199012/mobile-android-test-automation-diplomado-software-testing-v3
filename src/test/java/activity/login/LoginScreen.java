092fc05b45e91bebba86af8b0adcd7ef8b064f13
Juan De Dios <jbaudazio@gmail.com>
2023-09-18 22:38:43 -0500

    prueba de caso de prueba fallido en el pipeline

--- src/test/java/activity/login/LoginScreen.java
+++ src/test/java/activity/login/LoginScreen.java
@@ -5,7 +5,7 @@ import appiumControl.TextBox;
 import org.openqa.selenium.By;
 
 public class LoginScreen {
-    public TextBox empresaTextBox = new TextBox(By.id("com.uniflex.flexbusinessandroid:id/eteEmpresa"));
+    public TextBox empresaTextBox = new TextBox(By.id("aaaaacom.uniflex.flexbusinessandroid:id/eteEmpresa"));
     public TextBox usuarioTextBox = new TextBox(By.id("com.uniflex.flexbusinessandroid:id/loginEteUsuario"));
     public TextBox passwordTextBox = new TextBox(By.id("com.uniflex.flexbusinessandroid:id/LoginEtePassword"));
     public Button ingresarButton = new Button(By.id("com.uniflex.flexbusinessandroid:id/loginButIngresar"));
